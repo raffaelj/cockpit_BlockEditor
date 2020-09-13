146e053ff7d443d649f1c20916d360c6ea6ccc29
Raffael <github@raffaeljesche.de>
2020-09-13 12:05:55 +0200

    renamed addon to BlockEditor

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# Advanced layout field
+# Block editor addon for Cockpit CMS
 
-Addon for Cockpit CMS
+This addon replaces the layout and the layout-grid fields with a visual editor.
 
 **draft/experimental**
+
+
+## Installation
+
+Copy this repository into `/addons` and name it `BlockEditor` or
+
+```bash
+cd path/to/cockpit
+git clone https://github.com/raffaelj/cockpit_BlockEditor.git addons/BlockEditor
+```
--- admin.php
+++ admin.php
@@ -3,16 +3,16 @@
 $this->on('admin.init', function() {
 
     $this->helper('admin')->addAssets([
-        'layoutfieldadvanced:assets/css/style.min.css',
+        'blockeditor:assets/css/style.min.css',
 
-        'layoutfieldadvanced:assets/components/field-layout.tag',
-        'layoutfieldadvanced:assets/components/field-layout-grid.tag',
-        'layoutfieldadvanced:assets/components/layout-field-heading.tag',
-        'layoutfieldadvanced:assets/components/layout-field-image.tag',
-        'layoutfieldadvanced:assets/components/layout-field-text.tag',
-        'layoutfieldadvanced:assets/components/layout-field-divider.tag',
-        'layoutfieldadvanced:assets/components/layout-field-button.tag',
-        'layoutfieldadvanced:assets/components/layout-field-html.tag',
+        'blockeditor:assets/components/field-layout.tag',
+        'blockeditor:assets/components/field-layout-grid.tag',
+        'blockeditor:assets/components/layout-field-heading.tag',
+        'blockeditor:assets/components/layout-field-image.tag',
+        'blockeditor:assets/components/layout-field-text.tag',
+        'blockeditor:assets/components/layout-field-divider.tag',
+        'blockeditor:assets/components/layout-field-button.tag',
+        'blockeditor:assets/components/layout-field-html.tag',
     ]);
 
 });
